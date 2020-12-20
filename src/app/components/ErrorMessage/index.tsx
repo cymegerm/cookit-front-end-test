bb61f226aa7ce83152e67379e99ef0c3e46ea934
Simon St-Germain <cymegerm@gmail.com>
2020-12-20 05:26:26 -0500

    feat(error-message): create error-message component

---
+++ src/app/components/ErrorMessage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+export const ErrorMessage = (message: string) => {
+  return (
+    <Wrapper>
+      <Message>{message}</Message>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div;
+const Message = styled.p;
